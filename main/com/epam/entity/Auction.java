63f15bbd84a0cadbb33fe1a3ba63b45efe38e4b2
sandyukraine <Oleksandr_Kohut@epam.com>
2015-10-28 12:51:31 +0100

    - import statements added to entities - hibernate dependency added

--- main/com/epam/entity/Auction.java
+++ main/com/epam/entity/Auction.java
@@ -2,6 +2,13 @@ package com.epam.entity;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Entity
 @Table(name = "AUCTIONS")
 public class Auction {
