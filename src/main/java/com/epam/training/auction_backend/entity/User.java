13be7a26715b46e1f05c33035c3cb93c47f62f29
sandyukraine <Oleksandr_Kohut@epam.com>
2015-10-29 11:07:56 +0100

    - UserService added - try with resources used for session

--- src/main/java/com/epam/training/auction_backend/entity/User.java
+++ src/main/java/com/epam/training/auction_backend/entity/User.java
@@ -26,8 +26,7 @@ public class User {
 
 	}
 
-	public User(int id, String userName, String password) {
-		this.id = id;
+	public User(String userName, String password) {
 		this.userName = userName;
 		this.password = password;
 	}
