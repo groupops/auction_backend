7771d40611291e2c1a6f1982b481253f50c0241f
Yaroslav Kynyk <Yaroslav_Kynyk@epam.com>
2015-11-03 11:28:22 +0100

    added exception for auction not found case

--- src/main/java/com/epam/training/auction_backend/services/AuctionsServiceImpl.java
+++ src/main/java/com/epam/training/auction_backend/services/AuctionsServiceImpl.java
@@ -3,21 +3,23 @@ package com.epam.training.auction_backend.services;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-
 import com.epam.training.auction.common.UserTransferObject;
+import com.epam.training.auction_backend.exceptions.ItemNotFoundException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.epam.training.auction.common.AuctionTransferObject;
 import com.epam.training.auction.common.AuctionsService;
 import com.epam.training.auction_backend.entity.Auction;
 import com.epam.training.auction_backend.entity.User;
-import org.springframework.stereotype.Component;
+
+import static java.lang.String.format;
 
 @Service(value = "auctionsServiceImpl")
 public final class AuctionsServiceImpl implements AuctionsService {
     private static final Logger LOGGER = Logger.getLogger(AuctionsServiceImpl.class);
+    private static final String AUCTION_NOT_FOUND_MESSAGE = "Auction with id %d was not found";
+
     private AuctionRepository auctionRepository;
 
     @Autowired
@@ -43,14 +45,16 @@ public final class AuctionsServiceImpl implements AuctionsService {
     public Optional<AuctionTransferObject> getAuctionById(long id) {
         Auction auction = auctionRepository.findOne(id);
         Optional<AuctionTransferObject> auctionTransferObject;
+
         if (auction == null) {
-            auctionTransferObject = Optional.empty();
+            LOGGER.error(format(AUCTION_NOT_FOUND_MESSAGE, id));
+            throw new ItemNotFoundException(format(AUCTION_NOT_FOUND_MESSAGE, id));
         } else {
             User user = auction.getSellerUser();
             UserTransferObject userTransferObject = new UserTransferObject(user.getId(), user.getUserName(), user.getPassword());
             auctionTransferObject = Optional.of(AuctionTransferObject.getBuilder(auction.getTitle(), userTransferObject).build());
-
         }
+
         return auctionTransferObject;
     }
 }
